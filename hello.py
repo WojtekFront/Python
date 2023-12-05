f4bf2f597e42313c2215e3095a5d5417faf5eb2d
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2023-12-05 09:13:46 +0100

    first upload with the Variables

---
+++ hello.py
@@ -0,0 +1,18 @@
+
+#Praca z funkcjami:
+
+
+pierwszyTekst = "programować"
+z = float(3)
+if 3>2:
+    print("Zacznij "+pierwszyTekst)
+    print(type(z))
+   
+
+x= "?"
+def myfunc():
+  global x,y,z
+  x= "fantastic"
+
+myfunc()
+print("Python is " + x)
