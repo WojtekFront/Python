adb345f3c37a6e7fa9490052e3c6c096ed46e428
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2024-02-22 12:37:51 +0100

    learning Python

---
+++ index.py
@@ -0,0 +1,11 @@
+from math import *
+
+
+
+character_word = 'Fucking'
+
+print('hello '+ character_word.upper() +' world')
+print('this world is really '+ character_word.lower() +' awesome')
+print('I am born to create some cool stuff')
+print('So, I am '+ character_word +' rules that')
+
