215fce9b46948d5888c695e835763ab519f7fb70
Wojtek <wojciech_lisowski@wp.pl>
2024-02-22 13:02:06 +0100

    Test

--- index.py
+++ index.py
@@ -1,10 +1,8 @@
 from math import *
 
+character_word = 'Magic'
 
-
-character_word = 'Fucking'
-
-print('hello '+ character_word.upper() +' world')
+print('Hello '+ character_word.upper() +' world')
 print('this world is really '+ character_word.lower() +' awesome')
 print('I am born to create some cool stuff')
 print('So, I am '+ character_word +' rules that')
