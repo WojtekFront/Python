bf75982fb81c60bbb3754a6b9c037e1cae594bb5
Wojtek <wojciech_lisowski@wp.pl>
2024-02-23 12:45:12 +0100

    better callculator

--- calculator.py
+++ calculator.py
@@ -11,14 +11,14 @@ except ValueError:
     exit()
 
 try:
-    operator = str(input("Enter the operator (+,-,*,/): "))
+    operator = input("Enter the operator (+,-,*,/): ")
 except ValueError:
     print("The operator is not invalid. Please try again")
     exit()
 
-if num1.empty() or num2.empty() or operator.empty():
-    result = "incorrect input"
-elif num1 == 0 or num2 == 0 or operator == 0:
+# if num1.empty() or num2.empty() or operator.empty():
+#     result = "incorrect input"
+if num1 == 0 or num2 == 0 or operator == 0:
     result = 0
 elif operator == "+":
     result = num1 + num2
