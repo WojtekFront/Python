82478972cd78da575986fd1d4588fb4de1fb0d76
Wojtek <wojciech_lisowski@wp.pl>
2024-02-25 19:39:34 +0100

    test import

---
+++ testValue.py
@@ -0,0 +1,14 @@
+import random
+
+feet_in_miles = 5280
+meter_in_kilometers = 1000
+names = ["Mark", "Leon", "Gem", "Pair"]
+
+def get_file_ext(filename) :
+    return filename[filename.index(".")+1:]
+
+def roll_dice(num):
+    return random.randint(1, num)
+
+
+# print(get_file_ext("employees.txt"))
---
+++ usefulToolsImport.py
@@ -0,0 +1,5 @@
+import testValue as tv
+
+# print(tv.feet_in_miles) 
+
+print(tv.roll_dice(10)) 
