7e9a583a9e70698ec3870a9adfdfb5932661c59f
Wojtek <wojciech_lisowski@wp.pl>
2024-02-26 08:29:06 +0100

    improving files locations

---
+++ basic/hello.py
@@ -0,0 +1,18 @@
+
+#Work with functions:
+
+
+pierwszy_tekst = "programming"
+z = float(3)
+if 3>2:
+    print("Zacznij "+pierwszy_tekst)
+    print(type(z))
+   
+
+x= "?"
+def myfunc():
+  global x,y,z
+  x= "fantastic"
+
+myfunc()
+print("Python is " + x)
---
+++ work_with_python/1_library/Library.py
@@ -0,0 +1,15 @@
+class Library:
+
+        def __init__(self, book_title, book_author, book_description, book_type, book_year):
+                self.title_book = book_title
+                self.author_book = book_author
+                self.description_book = book_description
+                self.type_book = book_type
+                self.year_book = book_year
+        
+        
+
+
+
+
+
