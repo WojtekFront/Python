a14418e38b23ff641b62149ac2a0832acbaf348a
Wojtek <wojciech_lisowski@wp.pl>
2024-02-29 09:07:25 +0100

    Classes

--- learning/Classes.py
+++ learning/Classes.py
@@ -6,6 +6,9 @@ class Classes:
     def area(self):
         return self.width * self.height
     
+    def to_string(self):
+        return f'width: {self.width}, height: {self.height}'
+    
 new_classes = Classes(25, 3)
 
-print(new_classes.area())
+print(new_classes.t())
