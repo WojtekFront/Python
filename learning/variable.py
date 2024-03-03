1327efe7c433f4a211e79085008916c62deeb4c0
Wojtek <wojciech_lisowski@wp.pl>
2024-03-03 06:21:50 +0100

    work with variable

--- learning/variable.py
+++ learning/variable.py
@@ -1,3 +1,45 @@
+a= 10
+b=10
+
+a = [1,2,3]
+b = [1,2,3]
+print(a is b)
+print(a == b)
+
+
+a = [1,2,3]
+
+
+
+
+# Garbage Collection
+# import ctypes
+# import gc
+
+# def ref_count(address):
+#     return ctypes.c_long. from_address(address).value
+
+# def object_by_id(object_id):
+#     for obj in gc.get_objects():
+#         if id(obj) == object.id:
+#             return print("object exists")
+#     return print("object does not exist")
+
+# class A:
+#     def __init__(self):
+#         self.b = B(self)
+#         print(ref_count(self.b))
+
+# refernce counting
+# import sys
+
+# my_variable = 10
+# sys.getrefcount(my_variable)
+# print(sys.getrefcount(my_variable))
+
+
+
+
 # Variables and memory references
 a = 10
 # print(id(a))
