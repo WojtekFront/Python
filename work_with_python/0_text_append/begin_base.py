a861cd7fe356cf428e328043bb9a86c4f7cbce07
Wojtek <wojciech_lisowski@wp.pl>
2024-03-10 13:54:00 +0100

    work with Handling

--- work_with_python/0_text_append/begin_base.py
+++ work_with_python/0_text_append/begin_base.py
@@ -1,8 +1,15 @@
 import os
 
 os.chdir(r'C:\Pyt\Python\work_with_python\0_text_append')
-f = open("text.txt", "r")
 
-print(f.read())
 
-f.close()
+
+
+# print and append text to output
+# whole_text = open("text.txt", "r")
+# append_text = open("text.txt", "a")
+# append_text.write("This is new Python line")
+# for line_x in whole_text:
+#     print(line_x)
+
+# whole_text.close()
