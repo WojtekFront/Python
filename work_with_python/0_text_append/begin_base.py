07160e285251968dfc3f606b1c9410d7002455ac
Wojtek <wojciech_lisowski@wp.pl>
2024-03-10 14:44:48 +0100

    work with txt and Pandas

--- work_with_python/0_text_append/begin_base.py
+++ work_with_python/0_text_append/begin_base.py
@@ -1,15 +1,25 @@
 import os
+import datetime
 
 os.chdir(r'C:\Pyt\Python\work_with_python\0_text_append')
 
+def create_file_name(): 
+    return datetime.datetime.now().strftime('%y-%m-%d') + ".txt"
+
+# open the file
+try:
+    new_text = open(create_file_name(), 'x')
+except:
+    new_text = open(create_file_name(), 'r+')
 
 
+new_text.write("This is new Python line " + datetime.datetime.now().strftime('%H:%M:%S') + "\n")
+if new_text:
+    for line_x in new_text:
+        print(line_x)
+else:
+    print("We don't have anything")
 
-# print and append text to output
-# whole_text = open("text.txt", "r")
-# append_text = open("text.txt", "a")
-# append_text.write("This is new Python line")
-# for line_x in whole_text:
-#     print(line_x)
 
-# whole_text.close()
+# close the file
+new_text.close()
---
+++ work_with_python/6_pandas_library/learn_pandas.py
@@ -0,0 +1,2 @@
+#before begin project install Pandas. Write in console "pip install pandas"
+import pandas as pd
