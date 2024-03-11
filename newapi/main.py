0f9c731278b188232d86b9dd9a83ddaa76db0118
Wojtek <wojciech_lisowski@wp.pl>
2024-03-11 12:30:21 +0100

    flask

--- newapi/main.py
+++ newapi/main.py
@@ -1,31 +1,40 @@
 from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
 
 app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"data": "Hello World"}
+    def post(self):
+        return{"data": "new value"}
+    
+api.add_resource(HelloWorld,"/helloworld")
+
+# @app.route("/get-user/<user_id>")
+# def get_user(user_id):
+#     user_data = {
+#         "user_id": user_id,
+#         "name": "John Doe",
+#         "email": "john.doe@example.com"
+#     }
+#     extra = request.args.get("extra")
+#     if extra:
+#         user_data["extra"] = extra
+
+#     return jsonify(user_data), 200
+
+# @app.route("/create-user", methods=["POST"])
+# def create_user():
+#     if request.method =="POST":
+#         data = request.get_json()
+#         return jsonify(data), 201
 
 
-@app.route("/get-user/<user_id>")
-def get_user(user_id):
-    user_data = {
-        "user_id": user_id,
-        "name": "John Doe",
-        "email": "john.doe@example.com"
-    }
-    extra = request.args.get("extra")
-    if extra:
-        user_data["extra"] = extra
-
-    return jsonify(user_data), 200
-
-@app.route("/create-user", methods=["POST"])
-def create_user():
-    if request.method =="POST":
-        data = request.get_json()
-        return jsonify(data), 201
-
-
-@app.route("/")
-def home():
-    return "Home"
+# @app.route("/")
+# def home():
+#     return "Home"
 
 
 
