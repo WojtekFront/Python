0239dc540cc6e6f90f28489dd069b7071e9adda4
Wojtek <wojciech_lisowski@wp.pl>
2024-03-18 10:03:49 +0100

    list

--- learn/1_shots/lists.py
+++ learn/1_shots/lists.py
@@ -1,12 +1,25 @@
 # -----------------------------------------------------------------------------
-# -----------------------------------------------------------------------------
 digits_list = [22,11,21,45,21,45,21,45,21,45,21]
-strint_list = ["first_word", "second_word", "third_word", "fourth_word", "fifth_word", "sixth_word"]
-new_list = [*digits_list,*strint_list]
-new_list2 = [digits_list, strint_list]
+string_list = ["first_word", "second_word", "third_word", "fourth_word", "fifth_word", "sixth_word"]
+new_list = [*digits_list,*string_list]
+new_list2 = [digits_list, string_list]
+values_string = "abcdefghijklmnopqrstuvwxyzABCDEFGH"
+
+
+
+# print(string_list[2:])
+# for i in string_list:
+#     print(i[-2:])
+# print(values_string[-2:])
+
+# print(string_list[2:5])
+# print(string_list[-1])
+# print(new_string[-1])
+
+
+# -----------------------------------------------------------------------------
+
 # bracket_list = list((1,2,3 ))
 # print(bracket_list)
-
-
 # for element in digits_list.value():
     # print(element)
