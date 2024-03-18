1375f82855655d7d981e6147f3ab5d64755ad2c0
Wojtek <wojciech_lisowski@wp.pl>
2024-03-18 14:22:04 +0100

    list

--- learn/1_shots/lists.py
+++ learn/1_shots/lists.py
@@ -1,11 +1,35 @@
 # -----------------------------------------------------------------------------
-digits_list = [22,11,21,45,21,45,21,45,21,45,21]
+digits_list = [22, 11, 21, 45, 21, 45, 12, 21, 45, 21, 19, 45, 21]
 string_list = ["first_word", "second_word", "third_word", "fourth_word", "fifth_word", "sixth_word"]
 new_list = [*digits_list,*string_list]
 new_list2 = [digits_list, string_list]
 values_string = "abcdefghijklmnopqrstuvwxyzABCDEFGH"
+tuple_element = ("kiwi", "orange")
 
 
+# digits_list.clear()
+# del string_list, digits_list
+# for i in range(len(string_list)):
+#     print(string_list.pop(-1))
+# for i in digits_list:
+#     if i == 21:
+#         digits_list.remove(i)
+
+
+
+# print(string_list)
+# print(digits_list)
+
+# string_list[0] = 2
+# string_list.insert(0, "abcdefghij")
+# string_list.append("something")
+# string_list.extend(tuple_element)
+
+# print(string_list)
+
+# for i in digits_list:
+#     if i == 21:
+#         print("21 in digits_list") 
 
 # print(string_list[2:])
 # for i in string_list:
