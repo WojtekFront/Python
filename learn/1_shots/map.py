f091dc327f0fad1eef966bd359cc6ca6d55d31a8
Wojtek <wojciech_lisowski@wp.pl>
2024-03-19 12:49:36 +0100

    reducing

--- learn/1_shots/map.py
+++ learn/1_shots/map.py
@@ -1,14 +1,30 @@
-list1 = [1,2,3]
+
+list1 = [0,1,2,3,4,5,5,6,6,7,7,8,8,9,9]
 list2 = range(100)
 list3 = [4,5,6,7,8,9,10]
-list4 = [7,8,9,10,11,12,13,14]
+list4 = [7,8,92,10,11,12,13,14]
 
+import sys
+new_values = 0
+for x in list4:
+    print("x={0}".format(x))
+    print("new_values={0}".format(new_values))
+    if not x:
+        print("error)")
+        sys.exit()
+    elif new_values < x:
+        new_values = x
+print(new_values)     
+    
 
-newest_list = list(map(lambda x,y: x+y, list1, list3))
-print(newest_list)
-newest_list = [x+y for x,y in zip(list1, list3)]
-print(newest_list)
-newest_list= list(filter(lambda x: x%2==0, map(lambda x, y: x+y, list1, list3)))
+# newest_list = list(map(lambda x,y: x+y, list1, list3))
+
+# newest_list = [x+y for x,y in zip(list1, list3)]
+
+# newest_list= list(filter(lambda x: x%2==0, map(lambda x, y: x+y, list1, list3)))
+
+# newest_list = [x+y for x,y in zip(list1, list3)]
+# print(newest_list)
 
 
 # def sq(x):
