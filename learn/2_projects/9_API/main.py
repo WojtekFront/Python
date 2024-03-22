5c595ccaebe9566f3b6597f829bb7d3c594da7d7
Wojtek <wojciech_lisowski@wp.pl>
2024-03-22 07:18:36 +0100

    update gitignore and begin project api

---
+++ learn/2_projects/9_API/main.py
@@ -0,0 +1,8 @@
+import datetime as dt
+import requests # pip install requests
+
+# base_url = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
+base_url = 'https://api.openweathermap.org/data/3.0/weather?'
+
+city = "Szczecin"
+
