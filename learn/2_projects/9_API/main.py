ae0594335a24d96a245e6f302912a00f9ca8567a
Wojtek <wojciech_lisowski@wp.pl>
2024-03-22 07:53:45 +0100

    working api

--- learn/2_projects/9_API/main.py
+++ learn/2_projects/9_API/main.py
@@ -1,8 +1,18 @@
 import datetime as dt
 import requests # pip install requests
+import password 
 
-# base_url = 'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}'
-base_url = 'https://api.openweathermap.org/data/3.0/weather?'
+base_url = 'http://api.openweathermap.org/data/2.5/forecast?'
 
 city = "Szczecin"
+
+try:
+    url = base_url + "appid=" + password.api_key + "&q=" + city
+    response = requests.get(url).json()
+    print(response)
+except requests.exceptions.HTTPError as e:
+    print("wrong connectrion result: {0}".format(e))
+    exit(0)
+
+
 
