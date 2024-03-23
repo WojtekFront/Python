b2113625facb85e6f3711e28d0fb77257ea243f7
Wojtek <wojciech_lisowski@wp.pl>
2024-03-23 20:27:02 +0100

    better code

--- learn/2_projects/9_API/main.py
+++ learn/2_projects/9_API/main.py
@@ -1,5 +1,4 @@
 import datetime as dt
-import json
 import requests # pip install requests
 import password # external file with password
 
@@ -7,8 +6,9 @@ api_key = password.api_key
 city_name = "Szczecin"
 
 def kelvin_to_celsius(kelvin):
+    ''' Convert the given kelvin to celsius'''
     celsius = kelvin - 273.15
-    return int(celsius)
+    return int(celsius) # I don't know what rounding is used in the weather forecast
 
 try:
     base_url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={api_key}'
@@ -23,8 +23,8 @@ if response.status_code == 200:
     temp_in_celsius = kelvin_to_celsius(data['main']['temp'])
     feels_like_celsius = kelvin_to_celsius(data['main']['feels_like'])
     sunrise_time = dt.datetime.fromtimestamp(data['sys']['sunrise'])
+    # print(data)
 
-    print(data)
     # response data
     print("temperatura dzisiaj:      {0} C".format(temp_in_celsius))
     print("odczuwalna temperatura:   {0} C".format(feels_like_celsius))
