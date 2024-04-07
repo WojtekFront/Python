17b9b991ddd9f082171bd36aeebd634fea35b116
Wojtek <wojciech_lisowski@wp.pl>
2024-04-07 15:26:41 +0200

    partial

---
+++ learn/1_shots/partial_functions.py
@@ -0,0 +1,14 @@
+from functools import partial
+
+
+
+
+# def my_new_func(a,b,c):
+#     print(a,b,c)
+
+# f = lambda b,c: my_new_func(10,b,c)
+# # f(7,2)
+
+# f_p= partial(my_new_func, 1,22)
+# f_p(333)
+
