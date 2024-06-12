0d7a15e139d0f5d4bcb85446582c0bb3d97bfcab
Wojtek <wojciech_lisowski@wp.pl>
2024-06-12 10:18:59 +0200

    push

--- learn/2_projects/11_postgressql/4_query_functions_and_trigers.py
+++ learn/2_projects/11_postgressql/4_query_functions_and_trigers.py
@@ -258,7 +258,8 @@ try:
     #                 $$
     #                 LANGUAGE PLPGSQL""")
     # cursor.execute("""SELECT fn_get_sum_2(1,100);""")
-
+ 
+ 
     # cursor.execute(""" DROP FUNCTION IF EXISTS read_log() CASCADE;""")
     # cursor.execute(""" CREATE OR REPLACE FUNCTION read_log()
     #                     RETURNS TABLE(
