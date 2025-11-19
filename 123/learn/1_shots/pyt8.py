7d83ed7adcaf3a690a0969a87aab75d30cc91e35
Wojtek <42886283+WojtekFront@users.noreply.github.com>
2025-11-19 14:27:43 +0100

    learn

---
+++ 123/learn/1_shots/pyt8.py
@@ -0,0 +1,17 @@
+
+# casting 
+
+
+
+
+# print("Text", end="\n\n")
+# print("Drugi text")
+
+# test global variable
+# x = 'aa'
+# def globalFunc():
+#     global x
+#     x = "test"
+# x = "bb"
+# globalFunc()
+# print(x)
---
+++ 123/learn/1_shots/pyt9.py
@@ -0,0 +1,9 @@
+# a=0
+# while a<4:
+#     print(a, end =", ")
+#     a +=1
+# else:
+#     print("It is bigger")
+a=7
+if a < 0:
+    print("test")
